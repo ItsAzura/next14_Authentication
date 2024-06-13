8b8de8fd5520cc3a70673657a0660c0da5cae1a4
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-13 11:20:44 +0700

    chore: Refactor signOutUser function to remove token from cookie

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,37 @@
+'use client';
 import Image from 'next/image';
 import styles from './page.module.css';
 import { AuthUser } from '@/actions/user';
 import { redirect } from 'next/navigation';
+import { signOutUser } from '@/actions/user';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default async function Home() {
-  const user = await AuthUser();
-  console.log(user);
-  if (!user?.success) {
-    redirect('/sign-in');
-  }
+export default function Home() {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const fetchedUser = await AuthUser();
+      console.log(fetchedUser);
+      setUser(fetchedUser);
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleSignOut = async () => {
+    const result = await signOutUser();
+    router.push('/sign-in');
+  };
+
   return (
     <div>
       <h1>Next14 - Authentication</h1>
       <p>{user?.data?.username}</p>
       <p>{user?.data?.email}</p>
+      <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
 }
